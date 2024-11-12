af527dcc904f7eb9eff56b544cc277a84e8bac0a
Udayshankar Ravikumar <uralstech@gmail.com>
2024-11-12 18:41:55 +0530

    Moved to docfx, added COC and issue templates.

--- Runtime/Scripts/Exceptions/BhashiniAudioIOException.cs
+++ Runtime/Scripts/Exceptions/BhashiniAudioIOException.cs
@@ -3,12 +3,15 @@ using Uralstech.UBhashini.Data;
 
 namespace Uralstech.UBhashini.Exceptions
 {
+    /// <summary>
+    /// Exception thrown during IO-related audio processing failures.
+    /// </summary>
     public class BhashiniAudioIOException : Exception
     {
-        public BhashiniAudioIOException(string message, BhashiniAudioFormat format) : base($"Unsupported Bhashini API audio format \"{format}\": {message}") { }
+        internal BhashiniAudioIOException(string message, BhashiniAudioFormat format) : base($"Unsupported Bhashini API audio format \"{format}\": {message}") { }
 
-        public BhashiniAudioIOException(string message, Exception innerException) : base(message, innerException) { }
+        internal BhashiniAudioIOException(string message, Exception innerException) : base(message, innerException) { }
 
-        public BhashiniAudioIOException(string message) : base(message) { }
+        internal BhashiniAudioIOException(string message) : base(message) { }
     }
 }
